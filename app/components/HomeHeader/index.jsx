b781a1993ecabeabbcd8bf19e57929312dd4ea8c
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-25 19:23:25 +0800

    fix bug

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -10,12 +10,23 @@ class HomeHeader extends React.Component {
 
     render() {
         return (
-            <div>
+            <div id="home-header" className="clear-fix">
                 <div className="home-header-left float-left">
                     <span>{this.props.cityName}</span>
                     &nbsp;
                     <i className="icon-angle-down"></i>
                 </div>
+                <div className="home-header-right float-right">
+                    <i className="icon-user"></i>
+                </div>
+
+                <div className="home-header-middle">
+                    <div className="search-container">
+                        <i className="icon-search"></i>
+                        <input type="text" placeholder="请输入关键字"/>
+                    </div>
+                </div>
+
             </div>
         )
     }
