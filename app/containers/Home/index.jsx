6ea319bb384340ffaba463bf13f56e5109c3efd5
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-25 18:29:59 +0800

    主界面

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -9,7 +9,7 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h1>home</h1>
+                <h1>home111</h1>
             </div>
         )
     }
--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import LocalStore from '../util/localStore'
+import { CITYNAME } from '../config/localStoreKey'
+import * as userInfoActionsFromOtherFile from '../actions/userinfo'
+
 
 class App extends React.Component {
     constructor(props, context) {
@@ -10,14 +16,51 @@ class App extends React.Component {
         }
     }
     render() {
-        <div>
-            {
-                this.state.initDone
-                    ? this.props.children
-                    : <div>正在加载...</div>
-            }
-        </div>
+        return (
+            <div>
+                {
+                    this.state.initDone
+                        ? this.props.children
+                        : <div>正在加载...</div>
+                }
+            </div>
+        )
+    }
+    componentDidMount() {
+        // 获取位置信息
+        console.log('====componentDidMount=')
+        let cityName = LocalStore.getItem(CITYNAME)
+        if (cityName == null) {
+            cityName = '深圳';
+        }
+        console.log('====this.props.userInfoActions='+this.props.userInfoActions)
+        this.props.userInfoActions.update({
+            cityName: cityName
+        })
+
+        // 更改状态
+        setTimeout(() => {
+            this.setState({
+                initDone: true
+            })
+        }, 1000)
+
     }
 }
 
-export default App
+// -------------------redux react 绑定--------------------
+
+function mapStateToProps(state) {
+    return {
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch),
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App)
