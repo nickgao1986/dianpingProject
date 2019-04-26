3212e4de0070208dbb68329d128563b0a65f9e13
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-26 11:45:24 +0800

    search

--- app/containers/Search/index.jsx
+++ app/containers/Search/index.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
+import SearchHeader from '../../components/SearchHeader'
+import SearchList from './subpage/List'
+
 class Search extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
     render() {
+        const params = this.props.params
         return (
             <div>
-                <h1>Search</h1>
+                <SearchHeader keyword={params.keyword}/>
+                <SearchList keyword={params.keyword} category={params.type}/>
             </div>
         )
     }
-
-    componentDidMount() {
-        const params = this.props.params;
-        console.log('params=' + params.type);
-        console.log('params1=' + params.keyword);
-    }
 }
 
-// 使用 require.ensure 异步加载，还不支持 ES6 的 export 
-// export default Search
-module.exports = Search
+export default Search
---
+++ app/fetch/search/search.js
@@ -0,0 +1,8 @@
+import { get } from '../get'
+
+export function getSearchData(page, cityName, category, keyword) {
+    const keywordStr = keyword ? '/' + keyword : ''
+    console.log("keywordStr",keywordStr);
+    const result = get('/api/search/' + page + '/' + cityName + '/' + category + keywordStr)
+    return result
+}
